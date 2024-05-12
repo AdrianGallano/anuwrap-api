f5074de7396b29f38a4e700212dbe8b976d7b623
Adrian Gallano <adriangallanoengr@gmail.com>
2024-05-12 23:51:02 +0800

    Update autoload

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'f12e588bee039b687a38d0fa1758bb658c586f28',
+        'reference' => 'd59672f578fe047934128bd08979161abb864f54',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'f12e588bee039b687a38d0fa1758bb658c586f28',
+            'reference' => 'd59672f578fe047934128bd08979161abb864f54',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
